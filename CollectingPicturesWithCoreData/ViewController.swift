c3e8baca0a790a5c41abc1fde66331d1f6ef82a7
Josh Kinney <joshkinney@kinneykare.com>
2019-12-19 20:00:17 -0600

    Added ability to fetch CoreData to this project.

--- CollectingPicturesWithCoreData/ViewController.swift
+++ CollectingPicturesWithCoreData/ViewController.swift
@@ -7,15 +7,67 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var itemArray = [String]()
+    var idArray = [UUID]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self 
+        
+        //This sets up the barButton (Add)
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
         
+        getData()
     }
+    
+    
+    func getData() {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Photos")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            for result in results as! [NSManagedObject] {
+                if let name = result.value(forKey: "item") as? String {
+                    self.itemArray.append(name)
+                }
+                
+                if let id = result.value(forKey: "id") as? UUID {
+                    self.idArray.append(id)
+                }
+                self.tableView.reloadData()
+            }
+        } catch {
+            print("There was an errorVideo")
+        }
+        
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    
 
     @objc func addButtonTapped() {
         
